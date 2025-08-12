7d7df902f98ee302a48f5e15b8762697a8cba434
daekwon.park <daekwon.park@ten1010.io>
2025-08-12 15:55:08 +0900

    fix

--- src/main/java/io/ten1010/aipub/projectcontroller/domain/k8s/K8sObjectTypeConstants.java
+++ src/main/java/io/ten1010/aipub/projectcontroller/domain/k8s/K8sObjectTypeConstants.java
@@ -11,12 +11,6 @@ public final class K8sObjectTypeConstants {
                     ProjectApiConstants.VERSION,
                     ProjectApiConstants.PROJECT_RESOURCE_KIND),
             V1alpha1Project.class);
-    public static final K8sObjectType<V1alpha1NodeMaintenance> NODE_MAINTENANCE_V1ALPHA1 = new K8sObjectType<>(
-            new K8sObjectTypeKey(
-                    ProjectApiConstants.PROJECT_GROUP,
-                    ProjectApiConstants.VERSION,
-                    ProjectApiConstants.NODE_MAINTENANCE_RESOURCE_KIND),
-            V1alpha1NodeMaintenance.class);
     public static final K8sObjectType<V1alpha1NodeGroup> NODE_GROUP_V1ALPHA1 = new K8sObjectType<>(
             new K8sObjectTypeKey(
                     ProjectApiConstants.PROJECT_GROUP,
