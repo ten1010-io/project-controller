630ca896b77faa97d1c3c3e64c21ff2456924b38
Joonwoo Park <93jpark@gmail.com>
2026-03-03 10:20:15 +0900

    chore: image 1.4.0 tag

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 script_path=$(dirname "$0")
 
-./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=ten1010io/project-controller:0.1.0-SNAPSHOT
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=registry.ten1010.io:8443/project-controller:1.4.0
